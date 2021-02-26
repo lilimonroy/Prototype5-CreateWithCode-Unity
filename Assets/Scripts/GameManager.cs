1470c76b9df0887eb67f3c41d3366a413c8fcf25
lilimonroy <galadriel@comunidad.unam.mx>
2021-02-25 23:23:12 -0600

    fetch: Gameplay Mechanics & UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+
+    private float spawnRate = 1.0f;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScore(int scoreAdd)
+    {
+        score += scoreAdd;
+        scoreText.text = "Score: " + score;
+    }
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+
+    }
+}
